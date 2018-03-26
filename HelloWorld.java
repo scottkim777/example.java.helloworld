fa03a89254947a33abe371a29346c10c0d3a4d84
scottkim777 <37616090+scottkim777@users.noreply.github.com>
2018-03-26 15:56:24 +0800

    here you go...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World Scott");
+      System.out.println("Hello, World Scott.  Should be build 6");
    }
 }
